ce41883e7e1711cd6f8e8ec96f817105dd32c180
FerencKovacs1 <yepense@gmail.com>
2018-05-11 09:18:22 +0200

    After the wtf form debugging

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+
 
 
 app = Flask(__name__)
