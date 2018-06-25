da7e144a520d3f2cda48995347736c3896f6d520
Ferenc Kovacs <yepense@gmail.com>
2018-06-25 15:34:14 +0200

    wtf

--- static/java/stickynav.js
+++ static/java/stickynav.js
@@ -1,12 +1,20 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
+// Get the myTopnav
+var myTopnav = document.getElementById("myTopnav");
+
+// Get the offset position of the myTopnav
+var sticky = myTopnav.offsetTop;
+
+// Add the sticky class to the myTopnav when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    myTopnav.classList.add("sticky")
+  } else {
+    myTopnav.classList.remove("sticky");
+  }
+}
 
 // ===== Scroll to Top ====
 $(window).scroll(function() {
